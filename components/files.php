f011226c089301f9e76163749f340ec83ebde6fe
URIAN VIERA <urian1213viera@gmail.com>
2025-03-19 18:58:29 -0500

    cambiando diseño

--- components/files.php
+++ components/files.php
@@ -13,42 +13,44 @@ if ($list_files) {
             $preview = "<i class='bi $icon file-icon'></i>";
         } ?>
         <div data-search="<?php echo $archivo['nombre_original']; ?>" class="col-12 col-sm-6 col-md-4 col-lg-3 col-xl-2 file-item mb-3 m-2" data-id="<?php echo $archivo['id_drive']; ?>">
-            <div class="card h-100">
-                <div class="dropdown">
-                    <button class="btn file-menu p-0" type="button" id="fileMenu<?php echo $archivo['id_drive']; ?>"
-                        data-bs-toggle="dropdown" aria-expanded="false">
-                        <i class="bi bi-three-dots-vertical"></i>
-                    </button>
-                    <ul class="dropdown-menu" aria-labelledby="fileMenu<?php echo $archivo['id_drive']; ?>">
-                        <li>
-                            <a class="dropdown-item" href="<?php echo UPLOADS_PATH . $archivo['nombre_sistema']; ?>" target="_blank"><i
-                                    class="bi bi-box-arrow-up-right"></i>
-                                Abrir archivo
-                            </a>
-                        </li>
-                        <li>
-                            <a class="dropdown-item" download="<?php echo $archivo['nombre_sistema']; ?>"
-                                href="<?php echo DOWNLOADS_FILE; ?>?id_drive=<?php echo $archivo['id_drive']; ?>"><i class="bi bi-download"></i>
-                                Descargar
-                            </a>
-                        </li>
-                        <?php if ($id_directorio != '4') { ?>
-                            <li id="linkEnPapelera">
-                                <a class="dropdown-item eliminar-archivo" onclick="sendFileTrash('<?php echo $archivo['id_drive']; ?>')" href="#" data-id="<?php echo $archivo['id_drive']; ?>"
-                                    data-nombre="<?php echo htmlspecialchars($archivo['nombre_original']); ?>"><i
-                                        class="bi bi-trash"></i>
-                                    Enviar a la papelera
+            <div class="card h-100 shadow-sm animate-card">
+                <div class="card-header d-flex justify-content-between align-items-center">
+                    <span class="text-truncate"><?php echo $archivo['nombre_original']; ?></span>
+                    <div class="dropdown">
+                        <button class="btn file-menu p-0" type="button" id="fileMenu<?php echo $archivo['id_drive']; ?>"
+                            data-bs-toggle="dropdown" aria-expanded="false">
+                            <i class="bi bi-three-dots-vertical"></i>
+                        </button>
+                        <ul class="dropdown-menu" aria-labelledby="fileMenu<?php echo $archivo['id_drive']; ?>">
+                            <li>
+                                <a class="dropdown-item" href="<?php echo UPLOADS_PATH . $archivo['nombre_sistema']; ?>" target="_blank"><i
+                                        class="bi bi-box-arrow-up-right"></i>
+                                    Abrir archivo
                                 </a>
                             </li>
-                        <?php } ?>
-                    </ul>
+                            <li>
+                                <a class="dropdown-item" download="<?php echo $archivo['nombre_sistema']; ?>"
+                                    href="<?php echo DOWNLOADS_FILE; ?>?id_drive=<?php echo $archivo['id_drive']; ?>"><i class="bi bi-download"></i>
+                                    Descargar
+                                </a>
+                            </li>
+                            <?php if ($id_directorio != '4') { ?>
+                                <li id="linkEnPapelera">
+                                    <a class="dropdown-item eliminar-archivo" onclick="sendFileTrash('<?php echo $archivo['id_drive']; ?>')" href="#" data-id="<?php echo $archivo['id_drive']; ?>"
+                                        data-nombre="<?php echo htmlspecialchars($archivo['nombre_original']); ?>"><i
+                                            class="bi bi-trash"></i>
+                                        Enviar a la papelera
+                                    </a>
+                                </li>
+                            <?php } ?>
+                        </ul>
+                    </div>
                 </div>
                 <div class="card-body d-flex flex-column">
                     <a href="<?php echo UPLOADS_PATH . $archivo['nombre_sistema']; ?>" target="_blank" class="text-decoration-none text-dark">
                         <div class="file-preview mb-3 d-flex justify-content-center align-items-center" style="height: 130px;">
                             <?php echo $preview; ?>
                         </div>
-                        <p class="card-text text-truncate p-3"><?php echo $archivo['nombre_original']; ?></p>
                     </a>
                 </div>
             </div>
--- components/folders copy.php
+++ components/folders copy.php
@@ -5,11 +5,11 @@
     if (!empty($carpetasPorDirectorio)): ?>
         <?php foreach ($carpetasPorDirectorio as $folder):
             $isActive = ($folder['id_folder'] == $folderSelected) ? 'active-folder' : ''; ?>
+
             <section class="folder border p-3 mx-4 d-flex flex-column align-items-center text-center connected-list mb-3 <?= $isActive; ?>"
                 data-folder="<?= $folder['id_folder']; ?>">
-
-                <!-- Botón dropdown separado del enlace -->
-                <div class="align-self-end mb-2 dropdown">
+                <!-- Aquí el botón de dropdown, fuera del enlace -->
+                <div class="dropdown">
                     <button class="btn file-menu p-0" type="button" id="folderMenu<?= $folder['id_folder']; ?>"
                         data-bs-toggle="dropdown" aria-expanded="false">
                         <i class="bi bi-three-dots-vertical"></i>
@@ -20,24 +20,20 @@
                                 <i class="bi bi-box-arrow-up-right"></i> Publicar
                             </a>
                         </li>
+
                         <li>
                             <a class="dropdown-item" href="javascript:void(0);" onclick="toggleFolderVisibility(<?= $folder['id_folder']; ?>)">
                                 <i class="bi bi-lock"></i> Bloquear
                             </a>
                         </li>
-                        <li>
-                            <a class="dropdown-item" href="javascript:void(0);" onclick="toggleFolderVisibility(<?= $folder['id_folder']; ?>)">
-                                <i class="bi bi-trash"></i> Eliminar
-                            </a>
-                        </li>
                     </ul>
                 </div>
 
-                <a href="./index.php?dir=<?= $folder['id_folder']; ?>" class="text-decoration-none">
-                    <h4 class="icon fs-1">📁</h4>
-                    <h4 class="folder-name fs-5 mt-3 text-break"><?= htmlspecialchars($folder['nombre_folder']); ?></h4>
-                </a>
+                <h4 class="icon fs-1">📁</h4>
+                <h4 class="folder-name fs-5 mt-3 text-break"><?= htmlspecialchars($folder['nombre_folder']); ?></h4>
+
             </section>
+
         <?php endforeach; ?>
     <?php else: ?>
         <div class="col-md-12">
